bb27adcb0e6ab2755b15229c211a1068d5d75d57
Bnhrlol <133626453+Bnhrlol@users.noreply.github.com>
2023-09-08 10:35:02 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -15,6 +15,16 @@ function displayDiff() {
   document.getElementById("result").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
+function displayMult() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum * secondNum;
+
+  document.getElementById("manswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+}
+
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
 document.getElementById('diffButton').addEventListener("click", displayDiff);
+document.getElementById('multButton').addEventListener("click", displayMult);
